fe49a8b122ec7b464cc85b44c02286e915c37a21
Lakshya07-Goyal <lakshya.goyal.ug24@nsut.ac.in>
2025-05-17 00:31:14 +0530

    initial commit

---
+++ SUMOFTWONO.cpp
@@ -0,0 +1,12 @@
+#include<iostream>
+using namespace std;
+int main(){
+    int a,b;
+    cout<<"enter 1st no. = ";
+    cin>>a;
+    cout<<"enter 2st no. = ";
+    cin>>b;
+    int sum=a+b;
+    cout<<"sum of "<<a<<" and " <<b<<" is = "<<sum;
+    return 0;
+}
